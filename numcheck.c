0cffd811bec205448ea921ee035333a6e7f781b4
manoj81996 <manojpraveen.t.8.96@gmail.com>
2017-06-16 07:01:59 +0530

    Update numcheck.c

--- numcheck.c
+++ numcheck.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+int a;
+scanf("%d",&a);
+if(a!=0)
+{
+if(a>0)
+printf("positive");
+else
+printf("negative");
+}
+else
+printf("zero");
+}
